2b78a6237c6e7c2669090fd1ead473b7018e5bbd
yuanxizhang <yuanxi.zhang@gmail.com>
2019-02-11 02:03:33 -0800

    Done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,84 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  enable :sessions
+  
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(:name => params[:figure][:name])
+
+    @titles = Title.all
+    @figure.title_ids = params[:figure][:titles]
+    if !params[:figure][:title_name].empty?
+    	@figure.titles << Title.create(:name => params[:figure][:title_name])
+    end
+
+    @landmarks = Landmark.all
+    @figure.landmark_ids = params[:figure][:landmarks]
+    if !params[:figure][:landmark_name].empty?
+    	@figure.landmarks << Landmark.create(:name => params[:figure][:landmark_name], :year => params[:figure][:landmark_year])
+    end
+
+    @figure.save
+    redirect to "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    id = params[:id]
+    @figure = Figure.find_by_id(id)
+    erb :"figures/edit"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure][:name]
+    
+    @figure.title_ids = params[:figure][:titles]
+    if @figure.titles
+      @figure.titles.clear
+    end
+    titles = params[:figure][:titles]
+    titles.each do |title|
+      @figure.titles << Title.find(title)
+    end
+
+    if !params[:figure][:title_name].empty?
+    	@figure.titles << Title.create(:name => params[:figure][:title_name])
+    end
+
+    @figure.landmark_ids = params[:figure][:landmarks]
+    if @figure.landmarks
+      @figure.landmarks.clear
+    end
+
+    landmarks = params[:figure][:landmarks]
+    landmarks.each do |landmark|
+      @figure.landmarks << Landmark.find(landmark)
+    end
+
+    if !params[:figure][:landmark_name].empty?
+    	@figure.landmarks << Landmark.create(:name => params[:figure][:landmark_name], :year_completed => params[:figure][:landmark_year])
+    end
+
+    @figure.save
+    
+    redirect to "/figures/#{@figure.id}"
+  end
+ 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,45 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  enable :sessions
+  
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(:name => params[:landmark][:name], :year => params[:landmark][:year_completed])
+    @landmark.figure = Figure.find_or_create_by(:name => params[:landmark][:figure])
+    @landmark.save
+
+    redirect to "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.figure = Figure.find_or_create_by(name: params[:landmark][:figure])
+    @landmark.save
+    
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    id = params[:id]
+    @landmark = Landmark.find_by_id(id)
+    erb :"landmarks/edit"
+  end
 end
